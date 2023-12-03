04f731314dcf15dc9f826c1e49939c3b23b8d596
Jordi van der Hulst <jordivdh@hotmail.com>
2023-12-03 19:12:02 +0100

    Added empty project and talky plugin

---
+++ ThinkyJamProject/Source/ThinkyJamProject/ThinkyJamProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThinkyJamProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AThinkyJamProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AThinkyJamProjectGameMode();
+};
+
+
+
